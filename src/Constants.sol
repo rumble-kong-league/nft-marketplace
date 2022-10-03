04230e083f76fb65510858be78655f40fe4021b6
filkny <filip@rl.games>
2022-10-03 18:48:30 +0200

    Created Order struct, implemented trigger order logic, added single unit test for trigger order flow, added ERC20 and ERC721 mock tokens

---
+++ src/Constants.sol
@@ -0,0 +1,13 @@
+// Signature-related
+bytes32 constant EIP2098_allButHighestBitMask = (
+    0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+);
+
+uint256 constant EIP712_DomainSeparator_offset = 0x02;
+uint256 constant EIP712_OrderHash_offset = 0x22;
+uint256 constant EIP712_DigestPayload_size = 0x42;
+
+uint256 constant EIP712_PREFIX = (
+    0x1901000000000000000000000000000000000000000000000000000000000000
+);
+
