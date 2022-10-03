394f91572f0aeff6cbbcb17242c73af7e2d2005a
filkny <filip@rl.games>
2022-10-03 20:05:05 +0200

    Added signatureVerifier and corresponding unit tests, modified existing tests for marketplace

--- src/Constants.sol
+++ src/Constants.sol
@@ -11,3 +11,21 @@ uint256 constant EIP712_PREFIX = (
     0x1901000000000000000000000000000000000000000000000000000000000000
 );
 
+// Derive hash of the name of the contract.
+bytes32 constant _NAME_HASH = keccak256(bytes("RKL Marketplace"));
+
+// Derive hash of the version string of the contract.
+bytes32 constant _VERSION_HASH = keccak256(bytes("1"));
+
+// Construct the primary EIP-712 domain type string.
+// prettier-ignore
+bytes32 constant _EIP_712_DOMAIN_TYPEHASH = keccak256(
+    abi.encodePacked(
+        "EIP712Domain(",
+            "string name,",
+            "string version,",
+            "uint256 chainId,",
+            "address verifyingContract",
+        ")"
+    )
+);
